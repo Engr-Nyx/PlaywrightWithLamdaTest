215338eb068ce4676843239023b721a3a50b29ee
Engr-Nyx <arcee.tomaquin@gmail.com>
2026-07-11 21:06:32 +0800

    Use native browsers on HyperExecute VMs to prevent remote grid queuing

--- playwright.config.ts
+++ playwright.config.ts
@@ -85,59 +85,36 @@ export default defineConfig({
   },
 
   /* Configure projects for major browsers */
-  projects: [
-    ...lambdaTestCombos.map((combo) => ({
-      name: combo.projectName,
-      use: {
-        connectOptions: {
-          wsEndpoint: `wss://cdp.lambdatest.com/playwright?capabilities=${encodeURIComponent(
-            JSON.stringify(
-              getCapability({
-                platform: combo.platform,
-                browserName: combo.browserName,
-                browserVersion: combo.browserVersion,
-                name: `Playwright Test - ${combo.projectName}`,
-              })
-            )
-          )}`,
+  projects: process.env.HYPEREXECUTE === 'true'
+    ? [
+        {
+          name: 'chromium',
+          use: { ...devices['Desktop Chrome'] },
         },
-      },
-    })),
-    // {
-    //   name: 'chromium',
-    //   use: { ...devices['Desktop Chrome'] },
-    // },
-
-    // {
-    //   name: 'firefox',
-    //   use: { ...devices['Desktop Firefox'] },
-    // },
-
-    // {
-    //   name: 'webkit',
-    //   use: { ...devices['Desktop Safari'] },
-    // },
-
-    /* Test against mobile viewports. */
-    // {
-    //   name: 'Mobile Chrome',
-    //   use: { ...devices['Pixel 5'] },
-    // },
-    // {
-    //   name: 'Mobile Safari',
-    //   use: { ...devices['iPhone 12'] },
-    // },
-
-    /* Test against branded browsers. */
-    // {
-    //   name: 'Microsoft Edge',
-    //   use: { ...devices['Desktop Edge'], channel: 'msedge' },
-    // },
-    // {
-    //   name: 'Google Chrome',
-    //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
-    // },
-  ],
+        {
+          name: 'firefox',
+          use: { ...devices['Desktop Firefox'] },
+        },
+      ]
+    : [
+        ...lambdaTestCombos.map((combo) => ({
+          name: combo.projectName,
+          use: {
+            connectOptions: {
+              wsEndpoint: `wss://cdp.lambdatest.com/playwright?capabilities=${encodeURIComponent(
+                JSON.stringify(
+                  getCapability({
+                    platform: combo.platform,
+                    browserName: combo.browserName,
+                    browserVersion: combo.browserVersion,
+                    name: `Playwright Test - ${combo.projectName}`,
+                  })
+                )
+              )}`,
+            },
+          },
+        })),
+      ],
 
   /* Run your local dev server before starting the tests */
   // webServer: {
